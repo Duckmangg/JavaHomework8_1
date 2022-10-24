19d00f1e2cd4118e70c331bc72b0acc6082f4a97
Gleb <g.klochkov@worksolutions.ru>
2022-10-24 19:59:20 +0300

    test2

--- src/main/java/ru/netology/stats/Conditioner.java
+++ src/main/java/ru/netology/stats/Conditioner.java
@@ -1,2 +1,17 @@
-package ru.netology.stats;public class Condicioner {
+package ru.netology.stats;
+
+public class Conditioner {
+    public  int currentTemperature;
+    public void setCurrentTemperature(int newCurrentTemperature){
+        if (newCurrentTemperature < 0){
+            return;
+        }
+        if (newCurrentTemperature > 30){
+            return;
+        }
+        currentTemperature = newCurrentTemperature;
+    }
+    public int getCurrentTemperature(){
+        return currentTemperature;
+    }
 }
--- src/test/java/ru/netology/stats/test/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/test/StatsServiceTest.java
@@ -1,2 +1,21 @@
-package ru.netology.stats.test;public class StatsServiceTest {
+package ru.netology.stats.test;
+
+import org.junit.jupiter.api.Assertions;
+import ru.netology.stats.Conditioner;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class StatsServiceTest {
+    @Test
+    public void totalSalesOfAllMonthsTest() {
+        Conditioner cond = new Conditioner();
+        cond.setCurrentTemperature(40);
+        int actual = cond.getCurrentTemperature();
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
 }
