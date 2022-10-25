530f169a15a9909ea2c5f3a82771328c88d82e4a
Gleb <g.klochkov@worksolutions.ru>
2022-10-25 13:19:49 +0300

    test4

--- src/main/java/ru/netology/stats/Radio.java
+++ src/main/java/ru/netology/stats/Radio.java
@@ -1,2 +1,61 @@
-package ru.netology.stats;public class Radio {
+package ru.netology.stats;
+
+public class Radio {
+    public int currentStation;
+    public int currentVolume;
+
+    public void setStation(int station) {
+        currentStation = station;
+    }
+
+    public void setVolume(int volume) {
+        currentVolume = volume;
+    }
+
+    public void nextStation() {
+        if (currentStation < 9) {
+            currentStation = currentStation + 1;
+            return;
+        }
+        currentStation = 0;
+    }
+
+    public void prevStation() {
+        if (currentStation > 0) {
+            currentStation = currentStation - 1;
+            return;
+        }
+        currentStation = 9;
+    }
+
+    public void setCurrentStation(int newCurrentStation) {
+        if (newCurrentStation < 0) {
+            return;
+        }
+        if (newCurrentStation > 9) {
+            return;
+        }
+        currentStation = newCurrentStation;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+
+    }
+
+    public void reduceVolume() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
 }
--- src/test/java/ru/netology/stats/test/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/test/StatsServiceTest.java
@@ -3,13 +3,14 @@ package ru.netology.stats.test;
 import org.junit.jupiter.api.Assertions;
 import ru.netology.stats.Conditioner;
 import org.junit.jupiter.api.Test;
+import ru.netology.stats.Radio;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class StatsServiceTest {
     @Test
-    public void totalSalesOfAllMonthsTest() {
+    void changeConditioner40Test() {
         Conditioner cond = new Conditioner();
         cond.setCurrentTemperature(40);
         int actual = cond.getCurrentTemperature();
@@ -18,4 +19,159 @@ public class StatsServiceTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    void changeConditioner25Test() {
+        Conditioner cond = new Conditioner();
+        cond.setCurrentTemperature(25);
+        int actual = cond.getCurrentTemperature();
+        int expected = 25;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void changeConditionerNegativeTest() {
+        Conditioner cond = new Conditioner();
+        cond.setCurrentTemperature(-8);
+        int actual = cond.getCurrentTemperature();
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void nextRadioStation3Test() {
+
+        Radio rad = new Radio();
+        int station = 3;
+
+        rad.setStation(station);
+        rad.nextStation();
+
+        int actual = rad.getCurrentStation();
+        int expected = 4;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void nextRadioStation9Test() {
+
+        Radio rad = new Radio();
+        int station = 9;
+
+        rad.setStation(station);
+        rad.nextStation();
+
+        int actual = rad.getCurrentStation();
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void prevRadioStation0Test() {
+        Radio rad = new Radio();
+        int station = 0;
+        rad.setStation(station);
+        rad.prevStation();
+        int actual = rad.getCurrentStation();
+        int expected = 9;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void prevRadioStation3Test() {
+        Radio rad = new Radio();
+        int station = 3;
+        rad.setStation(station);
+        rad.prevStation();
+        int actual = rad.getCurrentStation();
+        int expected = 2;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void setRadioStation10Test() {
+        Radio rad = new Radio();
+        int station = 0;
+        rad.setStation(station);
+        rad.setCurrentStation(10);
+        int actual = rad.getCurrentStation();
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void setRadioStation5Test() {
+        Radio rad = new Radio();
+        int station = 0;
+        rad.setStation(station);
+        rad.setCurrentStation(5);
+        int actual = rad.getCurrentStation();
+        int expected = 5;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void setRadioStationNegativeTest() {
+        Radio rad = new Radio();
+        int station = 0;
+        rad.setStation(station);
+        rad.setCurrentStation(-5);
+        int actual = rad.getCurrentStation();
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void increaseVolumeRadio10Test() {
+        Radio rad = new Radio();
+        int volume = 10;
+        rad.setVolume(volume);
+        rad.increaseVolume();
+        int actual = rad.getCurrentVolume();
+        int expected = 10;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void increaseVolume5Test() {
+        Radio rad = new Radio();
+        int volume = 5;
+        rad.setVolume(volume);
+        rad.increaseVolume();
+        int actual = rad.getCurrentVolume();
+        int expected = 6;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void reduceVolumeRadio0Test() {
+        Radio rad = new Radio();
+        int volume = 0;
+        rad.setVolume(volume);
+        rad.reduceVolume();
+        int actual = rad.getCurrentVolume();
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void reduceVolume10Test() {
+        Radio rad = new Radio();
+        int volume = 10;
+        rad.setVolume(volume);
+        rad.reduceVolume();
+        int actual = rad.getCurrentVolume();
+        int expected = 9;
+
+        assertEquals(expected, actual);
+    }
 }
